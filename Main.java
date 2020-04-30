086cd518b941fc6b321bfeae0b639d144cdcdd82
Xiaoyang Song <56795191+xsong65@users.noreply.github.com>
2020-04-29 20:50:53 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Filename:   Main.java
  * Project:    a2
  * Authors:    William Mulvaney, Xiaoyang Song, Yinzhou Zhang
@@ -35,8 +35,9 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
- * Main class that runs the GUI. Uses objects inheriting from JavaFX's node 
+ * Main class that runs the GUI. Uses objects inheriting from JavaFX's node
  * class.
+ * 
  * @author williammulvaney
  *
  */
@@ -46,7 +47,7 @@ public class Main extends Application {
 	private static final int WINDOW_WIDTH = 400;
 	private static final int WINDOW_HEIGHT = 300;
 	private static final String APP_TITLE = "Milk Weights";
-	
+
 	private static Scene HomeScene;
 	private static Scene addFile;
 	private static Scene editFile;
@@ -56,10 +57,9 @@ public class Main extends Application {
 	private static Scene getMonthReport;
 	private static Scene getAnnualReport;
 	private static Scene designer;
-	
+
 	private static Manager manager;
 	private static Factory factory;
-	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -68,40 +68,36 @@ public class Main extends Application {
 
 		factory = new Factory();
 		manager = new Manager(factory);
-		
-		
+
 		// Create a vertical box with Hello labels for each args
 		VBox vbox = new VBox();
 
 		// Back Button that redirects to home page
 		Button backButton1 = new Button("Home");
-		backButton1.setOnAction(e -> primaryStage.setScene(HomeScene));	
-		
+		backButton1.setOnAction(e -> primaryStage.setScene(HomeScene));
+
 		Button backButton2 = new Button("Home");
-		backButton2.setOnAction(e -> primaryStage.setScene(HomeScene));	
-		
+		backButton2.setOnAction(e -> primaryStage.setScene(HomeScene));
+
 		Button backButton3 = new Button("Home");
-		backButton3.setOnAction(e -> primaryStage.setScene(HomeScene));	
-		
+		backButton3.setOnAction(e -> primaryStage.setScene(HomeScene));
+
 		Button backButton4 = new Button("Home");
-		backButton4.setOnAction(e -> primaryStage.setScene(HomeScene));	
-		
+		backButton4.setOnAction(e -> primaryStage.setScene(HomeScene));
+
 		Button backButton5 = new Button("Get Other Report");
 		backButton5.setOnAction(e -> primaryStage.setScene(getReport));
-		
+
 		Button backButton6 = new Button("Get Other Report");
 		backButton6.setOnAction(e -> primaryStage.setScene(getReport));
-		
+
 		Button backButton7 = new Button("Get Other Report");
 		backButton7.setOnAction(e -> primaryStage.setScene(getReport));
 
-		
 		// Home page buttons
 		Button button1 = new Button("Add File");
 		button1.setStyle("-fx-font-size: 2em; ");
 		button1.setOnAction(e -> primaryStage.setScene(addFile));
-		
-		
 
 		Button button2 = new Button("Get Report");
 		button2.setStyle("-fx-font-size: 2em; ");
@@ -110,14 +106,14 @@ public class Main extends Application {
 		Button button3 = new Button("Edit File");
 		button3.setStyle("-fx-font-size: 2em; ");
 		button3.setOnAction(e -> primaryStage.setScene(editFile));
-		
+
 		Button button4 = new Button("Designer");
 		button4.setStyle("-fx-font-size: 2em; ");
 		button4.setOnAction(e -> primaryStage.setScene(designer));
-		
+
 		Button quit = new Button("EXIT");
 		quit.setOnAction(e -> Platform.exit());
-		
+
 		vbox.getChildren().addAll(button1, button2, button3, button4, quit);
 
 		// File Path Text Box
@@ -127,30 +123,25 @@ public class Main extends Application {
 		Label fileNotFound = new Label("File not found");
 		VBox fileText = new VBox();
 		fileText.getChildren().addAll(filePrompt, insertFile, addFileButton);
-		
-			addFileButton.setOnAction( e->  {
-				try {
-					manager.readDataLineByLine(insertFile.getText());
-					if(fileText.getChildren().contains(fileNotFound)) 
-						fileText.getChildren().remove(fileNotFound);
-				} catch (Exception e1) {
-					// TODO Auto-generated catch block
-					fileText.getChildren().add(fileNotFound);
-				}
-			});
-		
-		
 
-		
+		addFileButton.setOnAction(e -> {
+			try {
+				manager.readDataLineByLine(insertFile.getText());
+				if (fileText.getChildren().contains(fileNotFound))
+					fileText.getChildren().remove(fileNotFound);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				fileText.getChildren().add(fileNotFound);
+			}
+		});
+
 		Label fileHeader = new Label("Add File");
 
 		BorderPane fileAdd = new BorderPane();
 		fileAdd.setCenter(fileText);
 		fileAdd.setTop(fileHeader);
 		fileAdd.setBottom(backButton1);
-		
 
-		
 		// Edit File Page
 		Label fileEditHeader = new Label("Edit File");
 		// Year field
@@ -174,14 +165,13 @@ public class Main extends Application {
 		date.getItems().addAll(dateArray);
 
 		VBox dateInsert = new VBox();
-		dateInsert.getChildren().addAll(yearLabel, year, monthLabel, month, dateLabel, date, editFileButton);
+		dateInsert.getChildren().addAll(yearLabel, year, monthLabel, month,
+				dateLabel, date, editFileButton);
 		BorderPane fileEdit = new BorderPane();
 		fileEdit.setTop(fileEditHeader);
 		fileEdit.setCenter(dateInsert);
 		fileEdit.setBottom(backButton2);
 
-		
-		
 		// Farm Report
 		Label title1 = new Label("Get Farm Report");
 		TextField farmID = new TextField();
@@ -191,59 +181,61 @@ public class Main extends Application {
 		Button buttonFR = new Button("Click here to get the report");
 		VBox vbox1 = new VBox();
 		CheckBox checkBox1 = new CheckBox("Create File");
-		vbox1.getChildren().addAll(title1, promptID, farmID, promptYr, yr, buttonFR, checkBox1, backButton3);
+		vbox1.getChildren().addAll(title1, promptID, farmID, promptYr, yr, buttonFR,
+				checkBox1, backButton3);
 		Label invalid = new Label("Invalid farm ID");
-		
+
 		TableView table = new TableView();
-		
-		buttonFR.setOnAction(e->{ 
+
+		buttonFR.setOnAction(e -> {
 			try {
-				String input = farmData(farmID.getText(), Integer.parseInt(yr.getText()));
-				if(input == null) {				
+				String input = farmData(farmID.getText(),
+						Integer.parseInt(yr.getText()));
+				if (input == null) {
 					vbox1.getChildren().add(invalid);
-				}
-				else if(!checkBox1.isSelected()) {
-					TableColumn monthColumn = new TableColumn("Month");	
-					monthColumn.setCellValueFactory(new PropertyValueFactory<Month, String>("month"));
-					
+				} else if (!checkBox1.isSelected()) {
+					TableColumn monthColumn = new TableColumn("Month");
+					monthColumn.setCellValueFactory(
+							new PropertyValueFactory<Month, String>("month"));
+
 					TableColumn totalColumn = new TableColumn("Total Weight");
-					totalColumn.setCellValueFactory(new PropertyValueFactory<Month, String>("totalWeight"));
-					if(table.getItems().size() == 0) {
+					totalColumn.setCellValueFactory(
+							new PropertyValueFactory<Month, String>("totalWeight"));
+					if (table.getItems().size() == 0) {
 						table.getColumns().addAll(monthColumn, totalColumn);
-						table.setItems(FXCollections.observableArrayList(new Month(1, 50, 5), new Month(1, 50, 5), new Month(1, 50, 5)));
+						table.setItems(FXCollections.observableArrayList(
+								new Month(1, 50, 5), new Month(1, 50, 5), new Month(1, 50, 5)));
 					}
 					vbox1.getChildren().add(table);
-				}
-				else {
+				} else {
 					manager.outputmonth(farmID.getText(), yr.getText());
 				}
-			
-			} catch(Exception f) {
-			
-		}
-		}	);
-			
-		
+
+			} catch (Exception f) {
+
+			}
+		});
+
 		// Get report
 		Label title = new Label("Get Report");
-		
+
 		Label prompt = new Label("Which report do you want?");
 		Button annual = new Button("Annual Report");
-		annual.setOnAction(e -> primaryStage.setScene(getAnnualReport));	
+		annual.setOnAction(e -> primaryStage.setScene(getAnnualReport));
 		Button monthly = new Button("Monthly Report");
 		monthly.setOnAction(e -> primaryStage.setScene(getMonthReport));
-		
+
 		Button dateRange = new Button("Date Range Report");
 		dateRange.setOnAction(e -> primaryStage.setScene(getDateRange));
-		
+
 		Button farmReportButton = new Button("Farm Report");
 		farmReportButton.setOnAction(e -> primaryStage.setScene(farmReport));
 
-
 		VBox vbox2 = new VBox();
 
-		vbox2.getChildren().addAll(farmReportButton, annual, monthly, dateRange, backButton4);
-		
+		vbox2.getChildren().addAll(farmReportButton, annual, monthly, dateRange,
+				backButton4);
+
 		// Get Date Range
 		Label DateRange = new Label("Date Range");
 		TextField insertdat = new TextField();
@@ -252,78 +244,78 @@ public class Main extends Application {
 		Label labelenddate = new Label("Enter End Date");
 		Button daterangebutton = new Button("Get report!");
 
-
 		VBox daterange = new VBox();
-		daterange.getChildren().addAll(DateRange, labeldat, insertdat, labelenddate, insertenddate, daterangebutton,
-				backButton5);
+		daterange.getChildren().addAll(DateRange, labeldat, insertdat, labelenddate,
+				insertenddate, daterangebutton, backButton5);
 
 		// Get Month Report
-        Label Monthreport = new Label("Month report");
-        TextField insertyearep = new TextField();
-        Label labelyearep = new Label("Enter Year(int)");
-        Label labelmonrep = new Label("Enter Month");
-        Button monthReportButton = new Button("Get Report!");
-        ComboBox<String> month1 = new ComboBox<String>();
-        CheckBox checkBox2 = new CheckBox("Create File");
-        month1.getItems().addAll(monthArray);
-        VBox monthDisplay= new VBox();
-        monthDisplay.getChildren().addAll(Monthreport,labelyearep, insertyearep, labelmonrep, month1,
-        		monthReportButton, checkBox2, backButton6);
-        
-        monthReportButton.setOnAction(e -> 	{
-        	try {
-        		//Table that will hold month information
-			
-        		TableView<FarmMonth> table1 = new TableView<FarmMonth>();			
-        		// If user has not selected box print table
-        		if(!checkBox2.isSelected()) {
-        			//First column
-        			TableColumn idColumn = new TableColumn("FarmID");	
-        			idColumn.setCellValueFactory(new PropertyValueFactory<Month, String>("farmID"));
-        			// Second column
-        			TableColumn totalColumn = new TableColumn("Total Weight");
-					totalColumn.setCellValueFactory(new PropertyValueFactory<Month, String>("totalWeight"));
+		Label Monthreport = new Label("Month report");
+		TextField insertyearep = new TextField();
+		Label labelyearep = new Label("Enter Year(int)");
+		Label labelmonrep = new Label("Enter Month");
+		Button monthReportButton = new Button("Get Report!");
+		ComboBox<String> month1 = new ComboBox<String>();
+		CheckBox checkBox2 = new CheckBox("Create File");
+		month1.getItems().addAll(monthArray);
+		VBox monthDisplay = new VBox();
+		monthDisplay.getChildren().addAll(Monthreport, labelyearep, insertyearep,
+				labelmonrep, month1, monthReportButton, checkBox2, backButton6);
+
+		monthReportButton.setOnAction(e -> {
+			try {
+				// Table that will hold month information
+				factory.getMonthlyReport(Integer.parseInt(insertyearep.getText()),
+						Integer.parseInt(month1.getValue()));
 				
+				TableView<FarmMonth> table1 = new TableView<FarmMonth>();
+				// If user has not selected box print table
+				if (!checkBox2.isSelected()) {
+					// First column
+					TableColumn idColumn = new TableColumn("FarmID");
+					idColumn.setCellValueFactory(
+							new PropertyValueFactory<Month, String>("farmID"));
+					// Second column
+					TableColumn totalColumn = new TableColumn("Total Weight");
+					totalColumn.setCellValueFactory(
+							new PropertyValueFactory<Month, String>("totalWeight"));
+
 					// Third column
 					TableColumn percentageColumn = new TableColumn("Percentage");
-					percentageColumn.setCellValueFactory(new PropertyValueFactory<FarmMonth, String>("percentage"));
-					//If it has not been added yet
-				
-					table.getItems().clear();
+					percentageColumn.setCellValueFactory(
+							new PropertyValueFactory<FarmMonth, String>("percentage"));
+					// If it has not been added yet
+
+					table1.getItems().clear();
 					table1.getColumns().addAll(idColumn, totalColumn, percentageColumn);
 					ArrayList<String> farmIDListMonth = factory.farmIDListMonthly;
 					ArrayList<Double> totalWeightMonth = factory.totalWeightMonthly;
 					ArrayList<Double> PercentageMonth = factory.PercentageMonthly;
-					ObservableList<Object> list = FXCollections.observableArrayList();
-					
+					ObservableList<FarmMonth> list = FXCollections.observableArrayList();
+
 					// Add all farms monthly reports
-					for(int i = 0; i < farmIDListMonth.size(); i++) {
-						list.add(new FarmMonth(totalWeightMonth.get(i), farmIDListMonth.get(i), PercentageMonth.get(i)));
+					for (int i = 0; i < farmIDListMonth.size(); i++) {
+						list.add(new FarmMonth(totalWeightMonth.get(i),
+								farmIDListMonth.get(i), PercentageMonth.get(i)));
 					}
-					table.setItems(list);
-					monthDisplay.getChildren().add(table);
-        		}		
-				else {
+					table1.setItems(list);
+					monthDisplay.getChildren().add(table1);
+				} else {
 					manager.outputmonth(farmID.getText(), yr.getText());
 				}
-	} catch(Exception f) {
-		
-		
-		}
-        });
-        
-        
+			} catch (Exception f) {
 
-        // Get Annual report
-        Label Annualreport = new Label("Annual report");
-        TextField insertyearep1 = new TextField();
-        Label labelyearep1 = new Label("Enter Years(int)");
-        Button annualReportButton = new Button("Get Report!");
-       
-        VBox yearDisplay = new VBox();
-        yearDisplay.getChildren().addAll(Annualreport,labelyearep1, insertyearep1, annualReportButton, backButton7);
-		
-        
+			}
+		});
+
+		// Get Annual report
+		Label Annualreport = new Label("Annual report");
+		TextField insertyearep1 = new TextField();
+		Label labelyearep1 = new Label("Enter Years(int)");
+		Button annualReportButton = new Button("Get Report!");
+
+		VBox yearDisplay = new VBox();
+		yearDisplay.getChildren().addAll(Annualreport, labelyearep1, insertyearep1,
+				annualReportButton, backButton7);
 
 		// Main layout is Border Pane example (top,left,center,right,bottom)
 		BorderPane root = new BorderPane();
@@ -337,16 +329,14 @@ public class Main extends Application {
 		root.setCenter(vbox);
 
 		// Home page scene
-		
-		Label designerLabel = new Label("ATEAM 94:\n Xiaoyang Song\n William Mulvaney\n Yinzhou Zhang");
+
+		Label designerLabel = new Label(
+				"ATEAM 94:\n Xiaoyang Song\n William Mulvaney\n Yinzhou Zhang");
 		Button backButton8 = new Button("Home");
-		backButton8.setOnAction(e -> primaryStage.setScene(HomeScene));	
+		backButton8.setOnAction(e -> primaryStage.setScene(HomeScene));
 		VBox designerPage = new VBox();
 		designerPage.getChildren().addAll(designerLabel, backButton8);
-		
-		
-		
-		
+
 		HomeScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 		// Add file page scene
 		addFile = new Scene(fileAdd, WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -362,7 +352,7 @@ public class Main extends Application {
 		getMonthReport = new Scene(monthDisplay, WINDOW_WIDTH, WINDOW_HEIGHT);
 		// Get annual report scene
 		getAnnualReport = new Scene(yearDisplay, WINDOW_WIDTH, WINDOW_HEIGHT);
-		//designer Scene
+		// designer Scene
 		designer = new Scene(designerPage, WINDOW_WIDTH, WINDOW_HEIGHT);
 
 		// Add the stuff and set the primary stage
@@ -377,34 +367,34 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	public static String farmData(String farmID, int year) {
-		if(factory.get(farmID) == null)
+		if (factory.get(farmID) == null)
 			return null;
 		String output = "Year: " + year + "\n Month    Total weight \n";
-		for(int i = 0; i < 12; i++) {
+		for (int i = 0; i < 12; i++) {
 			int sum = 0;
-			for(int j = 0; j < 31; j++) {
-				
-					sum += factory.get(farmID).get(year)[i][j];
+			for (int j = 0; j < 31; j++) {
+
+				sum += factory.get(farmID).get(year)[i][j];
 			}
-			output += (i+1) +  "    : " + sum + '\n';
+			output += (i + 1) + "    : " + sum + '\n';
 		}
 		return output;
 	}
-	
+
 	public class Month {
 		String month;
 		String totalWeight;
 		String percentage;
 		int total;
-		
+
 		public Month(int month, int totalWeight, int total) {
 			this.month = Integer.toString(month);
 			this.totalWeight = Integer.toString(totalWeight);
 			this.total = total;
 			this.percentage = "";
-		
+
 		}
 
 		/**
@@ -462,8 +452,6 @@ public class Main extends Application {
 		public void setTotal(int total) {
 			this.total = total;
 		}
-		
+
 	}
 }
-
-
